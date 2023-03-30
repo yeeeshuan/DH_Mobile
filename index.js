d85913b57f4c3e7e38c43528feaf3b8f622892f5
Ethan Huang <yehuang2020@gmail.com>
2023-03-30 15:17:13 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const btn = document.querySelector(".clicked");
+let mono = 1; 
+
+/*
+btn.addEventListener("click", changeColor);
+function changeColor() {
+    const count = 10; 
+  if (btn.style.background === "orange") {
+    btn.style.background = "white";
+  } else {
+    btn.style.background = "orange";
+  }
+}
+
+
+btn.addEventListener("click", myFunction_get);
+
+// Create a function for getting a variable value
+function myFunction_get() {
+    // Get the styles (properties and values) for the root
+    var temp = getComputedStyle(btn);
+    // Alert the value of the --blue variable
+    alert("The value of --mono is: " + temp.getPropertyValue('--mono'));
+    myFunction_set
+  }
+
+function myFunction_set() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    btn.style.setProperty('--mono', 1);
+  }
+
+function changeColor() {
+    mono = mono - 0.1; 
+    console.log(mono); 
+  if (mono > 0 ) {
+    btn.style.mono = mono;
+  } else {
+    mono = 1
+  }
+}
+
+*/
